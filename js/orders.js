bfe6da9ecd41d72e2dd332e7586d074b2309db19
Arsh <arshahmed235@gmail.com>
2023-05-21 17:13:44 +0530

    initial commit

---
+++ js/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '8945',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '5024',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'RUKO F11 Pro',
+        productNumber: '2742',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Drone With Camera Drone',
+        productNumber: '96992',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '40353',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Miami Swift Co',
+        productNumber: '8945',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Neon Mini Drone',
+        productNumber: '3945',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
